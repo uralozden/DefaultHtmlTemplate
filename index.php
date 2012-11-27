ce18438e58ac23552c2e1dbb98ee4afc6bd3f334
Ural ÖZDEN <uralozden@gmail.com>
2012-11-27 11:01:29 +0200

    html template uplad

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		
+		<title>empty html template</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta http-equiv="content-language" content="tr" />
+		<meta http-equiv="imagetoolbar" content="no" />
+		<meta name="robots" content="all,follow" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<!-- STYLESHEETS -->
+		<script type="text/javascript" src="asts/js/jquery.js"></script>
+		<script type="text/javascript" src="asts/js/main.js"></script>
+		<script type="text/javascript" src="asts/js/jquery_ui.js"></script>
+		<script type="text/javascript" src="asts/js/jcarousel.js"></script>		
+		<script type="text/javascript" src="asts/js/colorbox.js"></script>				
+		<script type="text/javascript" src="asts/js/watermark.js"></script>
+		<link rel="stylesheet" href="asts/css/global.css" type="text/css" media="screen"/>
+		<!--[if IE 8]><link rel="stylesheet" href="asts/css/ie8.css" type="text/css" media="screen"/><![endif]-->	
+</head>
+<body id="home">
+	<div id="container">
+
+		<div id="header">
+
+		<?php include "header.php";?>
+
+		</div><!--header-->
+
+		<div id="content">
+		
+		<?php include "content.php";?>
+
+		</div><!--content-->
+
+		<div id="footer">
+
+		<?php include "footer.php";?>
+
+		</div><!--footer-->
+
+	</div><!--container-->
+
+</body>
+</html>
